9f93e9bc5e2d4a4b63e79d18e0d235f125d1ec25
vaIegit <117516625+vaIegit@users.noreply.github.com>
2023-02-28 21:53:29 +0300

    Edit max charcters in phone number

--- visit_gen/main/models.py
+++ visit_gen/main/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class VisitCard(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
-    phone_number = models.CharField(max_length=12, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
     email = models.EmailField(blank=False)
     telegram = models.URLField(blank=True, max_length=255)
     vk = models.URLField(blank=True, max_length=255)
