a360054e21aa0c4354b5f81c02924f563b95836d
vaIegit <117516625+vaIegit@users.noreply.github.com>
2023-02-28 17:19:53 +0300

    Add first model and add to admin panel

--- visit_gen/main/models.py
+++ visit_gen/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class VisitCard(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    phone_number = models.CharField(max_length=12, blank=False)
+    email = models.EmailField(blank=False)
+    telegram = models.URLField(blank=True, max_length=255)
+    vk = models.URLField(blank=True, max_length=255)
+    youtube = models.URLField(blank=True, max_length=255)
+
+    def __str__(self):
+        return self.name
