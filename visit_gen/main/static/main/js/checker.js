260a67cac7ea97b42d2f160eb428c517872e2687
artem <aart7116@gmail.com>
2023-03-11 22:46:48 +0300

    i don't know what in this commit

--- visit_gen/main/static/main/js/checker.js
+++ visit_gen/main/static/main/js/checker.js
@@ -1,9 +1,6 @@
-var email = document.getElementById('E').textContent;
 var telegram = document.getElementById('T').textContent;
 var youtube = document.getElementById('Y').textContent;
 var vk = document.getElementById('V').textContent;
-
-document.getElementById("varElementE").innerHTML = '<i class="fa-solid fa-envelope"></i> ' + email;
 
 if (telegram != '') {
     document.getElementById("varElementT").innerHTML = '<i class="fa-brands fa-telegram"></i> ' + telegram;
