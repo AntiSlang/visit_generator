c3b7f6dc3ebc1bae4c54dc0b1a405624df295c66
artem <aart7116@gmail.com>
2023-03-11 18:19:56 +0300

    fixed some errors at main.js

--- visit_gen/main/static/main/js/main.js
+++ visit_gen/main/static/main/js/main.js
@@ -1,33 +1,33 @@
 let url = window.location.href;
 
-new QRCode(document.getElementById("qrcode"), url);
 
 // Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
-(function () {
-  'use strict'
+(function() {
+    'use strict'
 
-  // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
-  var forms = document.querySelectorAll('.needs-validation')
+// Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+var forms = document.querySelectorAll('.needs-validation')
 
-  // Зацикливайтесь на них и предотвращайте отправку
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+// Зацикливайтесь на них и предотвращайте отправку
+Array.prototype.slice.call(forms)
+    .forEach(function(form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
+            form.classList.add('was-validated')
+        }, false)
+})
 })()
 
 
 
 $(document).ready(function () {
     $(".phone").mask("+7(999)999-9999")
+    console.log("файл загружен")
 });
 
-
+new QRCode(document.getElementById("qrcode"), url);
 
